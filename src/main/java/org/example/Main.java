089ba8b0a0ab6014a6237d4841ca26f97d6856b1
Vitaliy <vitaliykulbaba@gmail.com>
2023-10-09 12:36:58 -0600

    Second commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,8 +3,8 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");
+        System.out.println("Hello People!");
+        System.out.println("Hello Women!");
+        System.out.println("Hello Men!");
     }
 }
